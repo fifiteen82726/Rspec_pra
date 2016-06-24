b366c5f41ece3a8776c158d58f0cc6a1518cc7cd
fifiteen82726 <fifiteen82726@gmail.com>
2016-06-25 01:13:25 +0800

    met trobles

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -7,7 +7,22 @@ describe Zombie do
 # 寫example 
 	it "is named Class_Ash" do
 	zombie = Zombie.new 
-	zombie.name.should == "Ash"
+	#zombie.name.should == "Ash"
+	expect(zombie.name).to eq "Ash"
     end
+
+
+    it "is there brain?" do
+    zombie = Zombie.new
+    zombie.brain.should < 1
+    end
+
+
+    it 'is hungry' do 
+    	zombie = Zombie.new
+    	zombie.should be_hungry
+    	# = zombie.hugry?.should ==true
+    end
+
 end
 
--- spec/zombie.rb
+++ spec/zombie.rb
@@ -1,6 +1,17 @@
-class Zombie
-	attr_accessor :name
+
+
+
+class Zombie < ActiveRecord::Base 
+	
+	validates :name, presence: true
+
+	attr_accessor :name, :brain
 	def initialize
 		@name = 'Ash'
+		@brain = 0
+	end
+
+	def hungry?
+		true
 	end
 end
